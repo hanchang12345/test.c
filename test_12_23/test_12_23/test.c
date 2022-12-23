a9990f4745b550958e0175cba356457e040cd901
hanchang12345 <120896886+hanchang12345@users.noreply.github.com>
2022-12-23 19:41:00 +0800

    Add files via upload

---
+++ test_12_23/test_12_23/test.c
@@ -0,0 +1,144 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<Windows.h>
+#include<stdlib.h>
+//int main()
+//{
+//	int i = 0;
+//	char password[20] = { 0 };
+//	
+//	for (i = 0; i < 3; i++)
+//	{
+//		printf("请输入密码:>");
+//		scanf("%s", password);
+//		if (strcmp(password ,"123456") == 0)
+// //==不能用来比较两个字符串是否相等
+// //应该用strcmp(password ,"123456") == 0比较--#include<string.h>
+//		{
+//			printf("登录成功\n");
+//			break;
+//		}
+//		else
+//		
+//			printf("密码错误，请重新输入"); 
+//	
+//			
+//	}
+//	if (i == 3)
+//		printf("三次输入均失败，退出程序");
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "welcome to changyi";
+//	char arr2[] = "##################";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;//----求右下标
+//				//strlen求字符串长度时不考虑\n
+//				//sizeof求字符串长度时考虑\n
+//	while (left<=right)
+//	{
+//		arr2[left] = arr1[left];
+//		arr2[right] = arr1[right];//---2中的字符被1中的字符取代
+//		printf("%s\n", arr2);
+//		Sleep(1000);//中间间隔1秒，()里是毫秒-#include<Windows.h>
+//		system("cls");//system是执行系统的函数--cls--清屏-#include<stdlib.h>
+//		left++;
+//		right--;
+//	}	
+//	printf("%s\n", arr2);
+//	return 0;
+//}
+
+
+//在有序的数组里找特定的数-- 二分法---最大次数 log₂n
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };//算元素个数
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int left = 0;//左下标
+//	int right = sz - 1;//算右下标
+//	while (left<=right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else
+//		{
+//			printf("找到了，下标是：%d\n", mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("找不到\n");
+//	}
+//	return 0;
+//}
+
+
+//在有序的数组里找特定的数-- 顺序方法（一个个找）
+//int main() 
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 5;
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (k == arr[i])
+//		{
+//			printf("找到了，下标是：%d\n", i);
+//			break;
+//		}
+//	}
+//	if (i == sz)
+//	{
+//		printf("没找到\n");
+//	}
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int z = 1;
+//	int i = 0;
+//	int sum = 0;
+//	for (n=1;n<=3;n++) n的阶乘和阶乘的和
+//	{
+//			z = z * n;
+//		 sum = sum + z;
+//	}
+//	printf("sum=%d\n", sum);
+//	printf("z=%d\n", z);
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	int z = 1;
+//	int i = 0;
+//	scanf("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		z = z * i;
+//		
+//	}
+//	printf("z=%d\n", z);
+//	return 0;
+//}
